7aa3f665ce3cbfe67e8f776229106e4122637a4f
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-03-11 12:39:47 +0530

    changes

--- search.js
+++ search.js
@@ -1,3 +1,81 @@
+
+// document.addEventListener("DOMContentLoaded", function () {
+//      var searchForm = document.getElementById("search Form");
+//      var searchInput = document.getElementById("search Input");
+//      var searchResults = document.getElementById("search Results");
+
+
+//      var products = [
+//           {
+//                category: "shorts",
+//                name: "The Way Home Shorts",
+//                image: "https://images.urbndata.com/is/image/Anthropologie/68438852_034_b2?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640",
+//                // link: "sproduct9.html",
+//           },
+
+//      ];
+
+//      searchForm.addEventListener("submit", function (event) {
+//           event.preventDefault();
+//           var searchTerm = searchInput.value.trim().toLowerCase();
+//           if (searchTerm !== "") {
+//                var filteredProducts = products.filter(function (product) {
+//                     return (
+//                          product.category.toLowerCase() === searchTerm ||
+//                          product.name.toLowerCase().includes(searchTerm)
+//                     );
+//                });
+
+//                displaySearchResults(filteredProducts)
+//           } else {
+//                hideSearchResults();
+//           }
+//      });
+
+//      function displaySearchResults(products) {
+//           searchResults.innerHTML = "";
+//           products.forEach(function (product) {
+//                var item = document.createElement("div");
+//                item.classList.add("searchItem");
+
+//                var image = document.createElement("img");
+//                image.src = product.image;
+//                image.alt = product.name;
+//                image.width = 70;
+
+//                var name = document.createElement("span");
+//                name.textContent = product.name;
+
+//                item.appendChild(image);
+//                item.appendChild(name);
+
+//                item.addEventListener("click", function () {
+//                     window.location.href = product.link;
+//                });
+
+//                searchResults.appendChild(item);
+//           });
+
+//           searchResults.classList.remove("hidden");
+//      }
+
+//      function hideSearchResults() {
+//           searchResults.innerHTML = "";
+//           searchResults.classList.add("hidden");
+//      }
+
+//      document.addEventListener("click", function (event) {
+//           if (!searchForm.contains(event.target)) {
+//                hideSearchResults();
+//           }
+//      });
+
+//      searchForm.addEventListener("click", function (event) {
+//           event.stopPropagation();
+//      });
+// });
+
+
 document.addEventListener("DOMContentLoaded", function () {
      const searchBar = document.querySelector(".search input");
      const cards = document.querySelectorAll(".card");
@@ -17,4 +95,3 @@ document.addEventListener("DOMContentLoaded", function () {
           });
      });
 });
-
